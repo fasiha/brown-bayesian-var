93eea4c1d9358d5bfc4778d46ef16e2b33c9d32f
Ahmed Fasih <wuzzyview@gmail.com>
2020-12-02 23:40:48 -0500

    Adding Brown's Bayesian VaR, modulo a sign flip?

--- var.py
+++ var.py
@@ -1,16 +1,20 @@
 import numpy as np
 import pandas as pd
-from scipy.stats import binom, ttest_ind
+from scipy.stats import binom, ttest_ind, norm
 
 # https://finance.yahoo.com/quote/%5EGSPC/history?p=%5EGSPC daily historical S&P 500 table
 df = pd.read_csv('^GSPC.csv', index_col='Date', parse_dates=True)
 
 df['pct_change'] = df['Close'].pct_change()
+# N.B.! sign change seems to be needed to match Brown? But that'll fit the wrong side of the distrubtion, right?
 
 p = 0.01  # var level
 nyears = 3
 
 window = round(5 / 7 * 365.25 * nyears)
+std_scalar = abs(norm.ppf(p))
+df['scaled_std'] = df['pct_change'].rolling(window).std().shift(1) * std_scalar
+
 df['hist_var'] = df['pct_change'].rolling(window).quantile(p).shift(1)
 # `shift` will push the roll one day into the future, so we can just look at a single row and see both the end-of-day PNL and start-of-day var prediction. See:
 # ```
@@ -37,8 +41,30 @@ def breaks_spacing(breaks: pd.Series, n: int, p: float):
 
 
 def breaks_levels(breaks: pd.Series, var: pd.Series):
-    return ttest_ind(var[breaks], var[~breaks], equal_var=False)
+    return dict(test=ttest_ind(var[breaks], var[~breaks], equal_var=False),
+                onbreak=var[breaks].mean(),
+                notbreak=var[~breaks].mean())
 
+
+def add_b_var(df):
+    df['b_var'] = df['scaled_std'].copy() * -1
+    b_var_idx = list(df.columns).index('b_var')
+    # loop over each adjacent pair of rows
+    for i in range(len(df) - 1):
+        a = df.iloc[i]
+        b = df.iloc[i + 1]
+        b_break = a['pct_change'] < a.b_var
+        if b_break:
+            df.iloc[i + 1, b_var_idx] = a.b_var * 2
+        else:
+            df.iloc[i + 1, b_var_idx] = 0.94 * a.b_var + 0.06 * (-b.scaled_std)
+    return df
+
+
+df = add_b_var(df)
+df['b_break'] = df['pct_change'] < df.b_var
+b_nbreaks = sum(df['b_break'])
+b_nbreaks_prob = binom.pmf(b_nbreaks, len(df), p)
 
 print(df)
 print(
@@ -48,4 +74,9 @@ print(
         hist_day2=breaks_spacing(df['hist_break'], 2, p),
         hist_day10=breaks_spacing(df['hist_break'], 10, p),
         hist_levels=breaks_levels(df.hist_break, df.hist_var),
+        b_nbreaks=b_nbreaks,
+        b_nbreaks_prob=b_nbreaks_prob,
+        b_day2=breaks_spacing(df.b_break, 2, p),
+        b_day10=breaks_spacing(df.b_break, 10, p),
+        b_levels=breaks_levels(df.b_break, df.b_var),
     ))
