247603e8c2ecb55d2082244d7aa82d0a8c9b0b4f
Ahmed Fasih <wuzzyview@gmail.com>
2020-12-03 21:26:30 -0500

    rename variables for clarity

--- var.py
+++ var.py
@@ -7,7 +7,7 @@ df = pd.read_csv('^GSPC.csv', index_col='Date', parse_dates=True)
 
 # df = df.iloc[:(19922 + 784 + 30)] # Try to replicate Brown's study exactly
 
-df['pct_change'] = df['Close'].pct_change()
+df['pnl'] = df['Close'].pct_change()
 # N.B.! sign change seems to be needed to match Brown? But that'll fit the wrong side of the distrubtion, right?
 
 p = 0.01  # var level
@@ -15,9 +15,9 @@ nyears = 3
 
 window = round(5 / 7 * 365.25 * nyears)
 std_scalar = abs(norm.ppf(p))
-df['scaled_std'] = df['pct_change'].rolling(window).std().shift(1) * std_scalar
+df['scaled_std'] = df.pnl.rolling(window).std().shift(1) * std_scalar
 
-df['hist_var'] = df['pct_change'].rolling(window).quantile(p).shift(1)
+df['hist_var'] = df.pnl.rolling(window).quantile(p).shift(1)
 # `shift` will push the roll one day into the future, so we can just look at a single row and see both the end-of-day PNL and start-of-day var prediction. See:
 # ```
 # pd.DataFrame(
@@ -28,7 +28,7 @@ df['hist_var'] = df['pct_change'].rolling(window).quantile(p).shift(1)
 # ```
 
 df = df.dropna()
-df['hist_break'] = df['pct_change'] < df.hist_var
+df['hist_break'] = df.pnl < df.hist_var
 hist_nbreaks = sum(df['hist_break'])
 hist_nbreaks_prob = binom.pmf(hist_nbreaks, len(df), p)
 
@@ -50,22 +50,32 @@ def breaks_levels(breaks: pd.Series, var: pd.Series):
 
 
 def add_b_var(df):
-    df['b_var'] = df['scaled_std'].copy() * -1
+    "Add Brown Bayesian VaR column to dataframe"
+    # Init b-var as the NEGATIVE scaled standard deviations
+    # This only matters for the first day
+    df['b_var'] = df.scaled_std.copy() * -1
+
+    # find the numeric index of b_var so we can overwrite it
     b_var_idx = list(df.columns).index('b_var')
-    # loop over each adjacent pair of rows
+
+    # loop over each adjacent pair of rows, except the very last row
     for i in range(len(df) - 1):
-        a = df.iloc[i]
-        b = df.iloc[i + 1]
-        b_break = a['pct_change'] < a.b_var
+        yesterday = df.iloc[i]
+        today = df.iloc[i + 1]
+        b_break = yesterday.pnl < yesterday.b_var
         if b_break:
-            df.iloc[i + 1, b_var_idx] = a.b_var * 2
+            df.iloc[i + 1, b_var_idx] = 2 * yesterday.b_var
         else:
-            df.iloc[i + 1, b_var_idx] = 0.94 * a.b_var + 0.06 * (-b.scaled_std)
+            df.iloc[
+                i + 1,
+                b_var_idx] = 0.94 * yesterday.b_var + 0.06 * -today.scaled_std
+            # Recall that `b.scaled_std` is the trailing scaled std as of this morning
+            # We're not cheating by using that here: it includes only yesterday's close
     return df
 
 
 df = add_b_var(df)
-df['b_break'] = df['pct_change'] < df.b_var
+df['b_break'] = df.pnl < df.b_var
 b_nbreaks = sum(df['b_break'])
 b_nbreaks_prob = binom.pmf(b_nbreaks, len(df), p)
 
@@ -90,7 +100,7 @@ plt.ion()
 plt.figure()
 df.b_var.plot(marker='.')
 (-1 * df.scaled_std).plot()
-df['pct_change'].plot()
+df.pnl.plot()
 plt.grid()
 
 df.to_json('df.json', orient='index', date_format='iso')
